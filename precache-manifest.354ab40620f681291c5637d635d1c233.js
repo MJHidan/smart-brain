578c0eb013a87aba87b8886b937a3114981f8bdd
MJHidan <mathiashidan@outlook.com>
2020-08-20 21:11:09 +0200

    Updates

---
+++ precache-manifest.354ab40620f681291c5637d635d1c233.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa1f1e1e0ba914d845e4d0dab4f06e29",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "076148c2b477162d471c",
+    "url": "/smart-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3bbe14e9cff751e0c16a",
+    "url": "/smart-brain/static/css/main.00f76579.chunk.css"
+  },
+  {
+    "revision": "076148c2b477162d471c",
+    "url": "/smart-brain/static/js/2.119cd597.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-brain/static/js/2.119cd597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bbe14e9cff751e0c16a",
+    "url": "/smart-brain/static/js/main.904c8813.chunk.js"
+  },
+  {
+    "revision": "640d180e9a19751583b3",
+    "url": "/smart-brain/static/js/runtime-main.1b1cfdb9.js"
+  }
+]);
